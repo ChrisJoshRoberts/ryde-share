d4bceb91506a944cd4277d2e012b434d50078507
Chris Roberts <chrisrobertscollective@gmail.com>
2024-10-01 21:32:13 +0200

    onboading screens created

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,63 @@
-import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import React, { useRef, useState } from "react";
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Swiper from "react-native-swiper";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Onboarding </Text>
-      </View>
+    <SafeAreaView className="flex h-full items-center bg-white justify-between">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View
+            className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]
+            rounded-full"
+          />
+        }
+        activeDot={
+          <View
+            className="w-[32px] h-[4px] mx-1 bg-[#0286FF]
+            rounded-full"
+          />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center mx-10 text-[#858585] mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
