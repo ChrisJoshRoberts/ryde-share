e53f84516dfa9b4f5b35bf38c43bd62d3a884611
Chris Roberts <chrisrobertscollective@gmail.com>
2024-10-12 19:26:21 +0200

    implementing clerk

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 import { SafeAreaView, Text, View } from "react-native";
 
 const Home = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
   return <Redirect href="/(auth)/welcome" />;
 };
 
